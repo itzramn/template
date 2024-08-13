353dc98fd444bc1e46710f883d84723f4deffb16
Itzel Ramon <itzelramonf@gmail.com>
2024-08-13 13:33:46 -0500

    add button for others login

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -69,6 +69,26 @@ export default function SignIn() {
           <Pressable className="p-4 bg-black rounded-full w-full items-center mb-4">
             <Text className=" text-white">Registrate</Text>
           </Pressable>
+          <View>
+            <Text className="text-center text-gray-400">
+              o inicia sesión con
+            </Text>
+            <View className="flex flex-row justify-center mt-4">
+              <Pressable className="flex flex-row p-4 rounded-full border border-slate-400 w-1/2 items-center justify-center mr-2">
+                <Image
+                  source={{
+                    uri: 'https://developers.google.com/static/identity/images/g-logo.png?hl=es-419',
+                  }}
+                  className="mr-2 w-6 h-6"
+                />
+                <Text className="font-bold">Google</Text>
+              </Pressable>
+              <Pressable className="flex flex-row p-4 rounded-full border border-slate-400  w-1/2 items-center justify-center">
+                <Ionicons name="logo-apple" size={20} color="black" />
+                <Text className="font-bold ml-2">Apple</Text>
+              </Pressable>
+            </View>
+          </View>
         </View>
       </View>
     </SafeAreaView>
