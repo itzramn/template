52a04c7b82b2f6a1a4f2858b969cfbf168230280
Diego <diegogutcat28@gmail.com>
2024-08-21 11:56:11 -0500

    SignUpForm now handles form logic

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,81 +1,10 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { useRouter } from 'expo-router';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { ThemedSafeAreaView } from '@/components/common/ThemedSafeAreaView';
-import { ThemedText } from '../components/common/ThemedText';
-import Button from '@/components/common/Button';
-import { AuthAPI } from '@/api/auth.api';
-import { FormFields, schema } from '@/schemas/signIn';
 import Form from '@/components/forms/SignUpForm';
-import ErrorText from '@/components/common/ErrorText';
-
-const authAPI = new AuthAPI();
 
 export default function SignUp() {
-  const router = useRouter();
-  const {
-    control,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-    setError,
-  } = useForm<FormFields>({
-    resolver: zodResolver(schema),
-  });
-  const onSubmit: SubmitHandler<FormFields> = async (data) => {
-    const result = await authAPI.signUp({
-      ...data,
-      role: 'admin',
-    });
-    if (!result.success) {
-      setError('root', { message: result.message });
-      return;
-    }
-    router.replace({
-      pathname: '/sign-in',
-      params: { registered: 'true' },
-    });
-  };
-
   return (
-    <ThemedSafeAreaView className="flex flex-1 justify-between">
-      <View className="pt-4 px-4">
-        <ThemedText
-          className="text-2xl text-primary-500"
-          style={{ fontFamily: 'QuicksandBold' }}
-        >
-          Regístrate
-        </ThemedText>
-        <Text
-          className="text-lg mb-4 text-gray-400"
-          style={{ fontFamily: 'Nunito' }}
-        >
-          Ingresa los datos que utilizarás para acceder a tu cuenta.
-        </Text>
-        <Form control={control} errors={errors} />
-      </View>
-      <View className="px-4 pb-2">
-        {errors.root?.message && (
-          <ErrorText txtClassName="mb-1" error={errors.root.message} />
-        )}
-        <Button
-          onPress={handleSubmit(onSubmit)}
-          isLoading={isSubmitting}
-          disabled={isSubmitting}
-        >
-          Continuar
-        </Button>
-
-        <Button
-          color="black"
-          variant="light"
-          txtClassName="my-4"
-          onPress={() => router.replace('/sign-in')}
-        >
-          Regresar
-        </Button>
-      </View>
+    <ThemedSafeAreaView className="flex flex-1">
+      <Form />
     </ThemedSafeAreaView>
   );
 }
--- components/forms/SignUpForm.tsx
+++ components/forms/SignUpForm.tsx
@@ -1,16 +1,91 @@
 import { useState } from 'react';
 import TextField from '../common/TextField';
-import { Control, FieldErrors } from 'react-hook-form';
-import { FormFields } from '@/schemas/signIn';
+import { Control, FieldErrors, SubmitHandler, useForm } from 'react-hook-form';
+import { FormFields, schema } from '@/schemas/signIn';
+import { useRouter } from 'expo-router';
+import { View, Text } from 'react-native';
+import ErrorText from '../common/ErrorText';
+import { ThemedText } from '../common/ThemedText';
+import Button from '../common/Button';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { AuthAPI } from '@/api/auth.api';
+
+const authAPI = new AuthAPI();
+
+export default function SignUpForm() {
+  const router = useRouter();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    setError,
+  } = useForm<FormFields>({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    const result = await authAPI.signUp({
+      ...data,
+      role: 'admin',
+    });
+    if (!result.success) {
+      setError('root', { message: result.message });
+      return;
+    }
+    router.replace({
+      pathname: '/sign-in',
+      params: { registered: 'true' },
+    });
+  };
+  return (
+    <View className="flex-1 justify-between">
+      <View className="pt-4 px-4">
+        <ThemedText
+          className="text-2xl text-primary-500"
+          style={{ fontFamily: 'QuicksandBold' }}
+        >
+          Regístrate
+        </ThemedText>
+        <Text
+          className="text-lg mb-4 text-gray-400"
+          style={{ fontFamily: 'Nunito' }}
+        >
+          Ingresa los datos que utilizarás para acceder a tu cuenta.
+        </Text>
+        <Fields control={control} errors={errors} />
+      </View>
+      <View className="px-4 pb-2">
+        {errors.root?.message && (
+          <ErrorText txtClassName="mb-1" error={errors.root.message} />
+        )}
+        <Button
+          onPress={handleSubmit(onSubmit)}
+          isLoading={isSubmitting}
+          disabled={isSubmitting}
+        >
+          Continuar
+        </Button>
+
+        <Button
+          color="black"
+          variant="light"
+          txtClassName="my-4"
+          onPress={() => router.replace('/sign-in')}
+        >
+          Regresar
+        </Button>
+      </View>
+    </View>
+  );
+}
 
 type Errors = FieldErrors<FormFields>;
 type InputControl = Control<FormFields, any>;
-type Props = {
+type FieldsProps = {
   control: InputControl;
   errors: Errors;
 };
 
-export default function SignUpForm({ control, errors }: Props) {
+function Fields({ control, errors }: FieldsProps) {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   return (
